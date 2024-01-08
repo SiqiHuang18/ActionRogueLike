e8d19e0963303cbfc649ff68590483dda5150018
siqihuang <siqihuang@faceunity.com>
2024-01-08 18:58:57 +0800

    add BT service; Modify Build.cs

---
+++ Source/ActionRogueLike/Private/AI/SBTService_CheckAttackRange.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTService_CheckAttackRange.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+
+void USBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	// check distance between ai pawn and target actor
+
+	UBlackboardComponent* BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	if (ensure(BlackBoardComp))
+	{
+		AActor* TargetActor = Cast<AActor>(BlackBoardComp->GetValueAsObject("TargetActor"));
+		if (TargetActor)
+		{
+			AAIController* MyController = OwnerComp.GetAIOwner();
+			if (ensure(MyController))
+			{
+				APawn* BotPawn = MyController->GetPawn();
+				if (ensure(BotPawn))
+				{
+					float distance = FVector::Distance(TargetActor->GetActorLocation(), BotPawn->GetActorLocation());
+
+					bool bWithinRange = distance < 1000.f;
+
+					BlackBoardComp->SetValueAsBool(AttackRangeKey.SelectedKeyName, bWithinRange);
+
+				}
+			}
+		}
+	}
+
+
+}
